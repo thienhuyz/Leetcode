be47b0db74078657bdea4cb106e087457d5fe82e
thienhuyz <thienhuyz3006@gmail.com>
2026-06-07 16:04:56 +0700

    2625.js

---
+++ 30 days js/Promises and Time/2625.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+var flat = function (arr, n) {
+    const result = [];
+
+    for (const item of arr) {
+        if (Array.isArray(item) && n > 0) {
+            result.push(...flat(item, n - 1));
+        } else {
+            result.push(item);
+        }
+    }
+    return result;
+};
