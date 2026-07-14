f309e98f46434959d6610d0312510b6d081f94dd
thienhuyz <thienhuyz3006@gmail.com>
2026-07-14 13:47:19 +0700

    Fix 167.js

--- 30 days js/Two pointers/167_TwoSumII.js
+++ 30 days js/Two pointers/167_TwoSumII.js
@@ -6,7 +6,7 @@
 var twoSum = function (numbers, target) {
     let left = 0;
     let right = numbers.length - 1;
-    while (left < numbers.length - 1 && right > 0) {
+    while (left < right) {
         if (numbers[left] + numbers[right] < target) left++;
         else if (numbers[left] + numbers[right] > target) right--;
         else return [left + 1, right + 1];
